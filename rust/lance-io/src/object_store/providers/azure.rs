9046423292444cfc88f453aaf61761db422a0128
forwardxu <forwardxu@apache.org>
2026-07-18 10:20:20 +0800

    feat: bump opendal to 0.58 via goosefs-sdk 0.1.7 fork
    - Replace opendal/object_store_opendal with XuQianJin-Stars/opendal@chore/bump-goosefs-sdk-to-0.1.7 (0.58.0)
    - Upgrade goosefs-sdk to 0.1.7 to satisfy opendal-service-goosefs >=0.1.7
    - Adapt lance-io object_store providers to opendal 0.58 Operator::new/from_iter
      returning Result<Operator> directly (drop .finish() builder step)

--- rust/lance-io/src/object_store/providers/azure.rs
+++ rust/lance-io/src/object_store/providers/azure.rs
@@ -106,14 +106,9 @@ impl AzureBlobStoreProvider {
                     config_map.insert("root".to_string(), format!("/{}", prefix));
                 }
 
-                Operator::from_iter::<Azblob>(config_map)
-                    .map_err(|e| {
-                        Error::invalid_input(format!(
-                            "Failed to create Azure Blob operator: {:?}",
-                            e
-                        ))
-                    })
-                    .map(|b| b.finish())
+                Operator::from_iter::<Azblob>(config_map).map_err(|e| {
+                    Error::invalid_input(format!("Failed to create Azure Blob operator: {:?}", e))
+                })
             }
             "abfss" => {
                 let filesystem = base_path.username();
@@ -139,14 +134,12 @@ impl AzureBlobStoreProvider {
                     config_map.insert("root".to_string(), format!("/{}", root_path));
                 }
 
-                Operator::from_iter::<Azdls>(config_map)
-                    .map_err(|e| {
-                        Error::invalid_input(format!(
-                            "Failed to create Azure DFS (ADLS Gen2) operator: {:?}",
-                            e
-                        ))
-                    })
-                    .map(|b| b.finish())
+                Operator::from_iter::<Azdls>(config_map).map_err(|e| {
+                    Error::invalid_input(format!(
+                        "Failed to create Azure DFS (ADLS Gen2) operator: {:?}",
+                        e
+                    ))
+                })
             }
             _ => Err(Error::invalid_input(format!(
                 "Unsupported Azure scheme: {}",
